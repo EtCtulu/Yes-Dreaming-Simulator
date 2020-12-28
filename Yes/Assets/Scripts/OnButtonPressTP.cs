588a50d6a0b7d6d7eaa9e16d5eac51dfdd923767
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2020-12-29 00:14:45 +0100

    Ajout de TMPro, ajout du fog, tweaks sur le FPS AIO, ajout de 2 types de Téléportations avec PH d'interraction.

---
+++ Yes/Assets/Scripts/OnButtonPressTP.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OnButtonPressTP : MonoBehaviour
+{
+    public Transform tpPoint;
+
+    private Transform playerTransform;
+    private GameObject _pressEText;
+    private bool _isInside = false;
+
+    private void Start()
+    {
+        _pressEText = GameObject.FindGameObjectWithTag("Press E");
+        _pressEText.SetActive(false);
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_isInside == true)
+            {
+                playerTransform.position = tpPoint.position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isInside = true;
+        _pressEText.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isInside = false;
+        _pressEText.SetActive(false);
+    }
+}
---
+++ Yes/Assets/Scripts/OnHoverTP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHoverTP : MonoBehaviour
+{
+    
+    public Transform tpPoint;
+
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerTransform.position = tpPoint.position;
+    }
+}
