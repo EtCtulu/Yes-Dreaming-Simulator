c34776f59d01f79f3571473622840d269be1b5d8
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2020-12-29 22:37:44 +0100

    Jej

---
+++ Yes/Assets/Scripts/EnemyLookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLookAtPlayer : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player Model").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, player.transform.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+}
