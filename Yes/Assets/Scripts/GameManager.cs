d7913a0b2b1f846876d9e5db14de6a3f0ffd88e0
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2020-12-29 01:08:10 +0100

    Ajout d'un début de Game Manager + création de tags

---
+++ Yes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    
+    private static GameManager _instance;
+
+    private int _eventDid = 0;
+    private GameObject _eventDidDraw;
+    private Text _eventDidDrawText;
+    
+    void Start()
+    {
+        
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            _eventDidDraw = GameObject.FindGameObjectWithTag("TextEvent");
+            _eventDidDrawText = _eventDidDraw.GetComponent<Text>();
+            _eventDidDraw.SetActive(false);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(OnEventActivate());
+        }
+    }
+
+    public IEnumerator OnEventActivate()
+    {
+        _eventDid++;
+        _eventDidDraw.SetActive(true);
+        _eventDidDrawText.text = ("You found " + _eventDid +" events out of 20");
+        yield return new WaitForSeconds(3f);
+        _eventDidDraw.SetActive(false);
+        yield return 0;
+    }
+    
+}
