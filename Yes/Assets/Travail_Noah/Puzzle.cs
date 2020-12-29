81b4801f3d2937ffaff8eccd91782b82f580704b
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2020-12-29 13:06:37 +0100

    ajout de deux event/puzzle

---
+++ Yes/Assets/Travail_Noah/Puzzle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public GameObject target;
+    public bool end=false;
+    public int id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (end)
+        {
+            Destroy(this.GetComponent<Rigidbody>());
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag!=null && other.tag == $"Target{id}")
+        {
+            this.gameObject.transform.position = target.transform.position;
+            this.gameObject.transform.rotation = target.transform.rotation;
+
+            end = true;
+        }
+    }
+}
---
+++ Yes/Assets/Travail_Noah/PuzzleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public List<GameObject> Blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Blocks[0].GetComponent<Puzzle>().end && Blocks[1].GetComponent<Puzzle>().end && Blocks[2].GetComponent<Puzzle>().end)
+        {
+            Debug.Log("event added");
+        }
+    }
+}
