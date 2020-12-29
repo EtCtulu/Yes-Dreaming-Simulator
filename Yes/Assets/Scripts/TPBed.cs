dc0bb06edebfda03cf73308fd12668892b62cb31
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2020-12-29 17:04:28 +0100

    On en voit la fin

---
+++ Yes/Assets/Scripts/TPBed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPBed : MonoBehaviour
+{
+    public GameObject sleepCamera;
+    public GameObject player;
+
+    private bool _isInside;
+    private GameObject _pressEText;
+
+    private void Start()
+    {
+        _pressEText = GameObject.FindGameObjectWithTag("Press E");
+        _pressEText.SetActive(false);
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_isInside == true)
+            {
+                Destroy(player);
+                sleepCamera.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isInside = true;
+        _pressEText.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isInside = false;
+        _pressEText.SetActive(false);
+    }
+}
