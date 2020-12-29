49e08b434d7382b4084355354f38469943a2f6d0
Emilien Bertrand <58692462+EtCtulu@users.noreply.github.com>
2020-12-29 20:57:49 +0100

    Jumpscare

---
+++ Yes/Assets/Scripts/JumpscareHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpscareHandler : MonoBehaviour
+{
+    public GameObject jumpScare;
+    public GameObject player;
+    public GameObject music;
+    public GameObject camera;
+    void Start()
+    {
+        StartCoroutine(Jumpscare());
+    }
+
+    
+
+    public IEnumerator Jumpscare()
+    {
+        yield return new WaitForSeconds(15);
+        Destroy(player);
+        camera.SetActive(true);
+        Destroy(music);
+        jumpScare.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        Application.Quit();
+        
+    }
+    
+}
