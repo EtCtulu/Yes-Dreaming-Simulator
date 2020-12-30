c4760ae15763ceb2fbdd95e11865cb8a2db9d2d4
Drasibaoh <71836998+Drasibaoh@users.noreply.github.com>
2020-12-30 14:55:31 +0100

    ajout de Sons

--- Yes/Assets/Travail_Noah/script/MovingItem.cs
+++ Yes/Assets/Travail_Noah/script/MovingItem.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class MovingItem : MonoBehaviour
 {
+    public List<AudioSource> animalsound;
     public int index = 0;
     public List<GameObject> positions;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {  
-        if (index==positions.Count)
+    {
+        if (index == positions.Count)
         {
-           
+
             Debug.Log("event added");
-            Destroy(this.gameObject);
             GameManager._instance.Coroutine();
+            Destroy(this.gameObject);
+            
         }
         this.transform.position = positions[index].transform.position;
-        
+
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag!=null && collision.gameObject.tag=="Player")
-        index += 1;
+        if (collision.gameObject.tag != null && collision.gameObject.tag == "Player" && index == positions.Count - 1)
+        {
+            index += 1;
+            animalsound[0].Play();
+            //joue son2
+        }
+        else if (collision.gameObject.tag != null && collision.gameObject.tag == "Player"&& index<positions.Count-1)
+        {
+            index += 1;
+            animalsound[1].Play();
+            //joue son1
+        }
     }
 }
